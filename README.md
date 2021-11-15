957dafe5d96ea9c466f0f485c66cc3f048335978
Sami Vauhkonen <sami.vauhkonen@gmail.com>
2021-11-16 01:40:13 +0200

    lisätty vko 2

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # OHTE Harjoitustyö
 
+[Vaatimusmäärittely]()
+
+
 ## Captain's Log
 **-302818.37** This is starting point for no returning trip to _OHTE_.
 
@@ -8,9 +11,16 @@ As Kanye West said:
 > We're living the future so
 > the present is our past. 
 
+
+
 ### Viikko 1
 
 [GitLog](https://github.com/Vauhkonen/ot-harjoitustyo/blob/master/laskarit/viikko1/gitlog.txt)
 
 
 [Komentorivi](https://github.com/Vauhkonen/ot-harjoitustyo/blob/master/laskarit/viikko1/komentorivi.txt)
+
+
+### Viikko 2
+
+
--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -3,7 +3,22 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        self.maksukortti = Maksukortti(10)
+        self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "saldo: 20.0")
+
+    def test_rahan_ottaminen_toimii_kun_saldo_riittaa(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "saldo: 5.0")
+
+    def test_saldo_ei_muutu_kun_rahaa_otetaan_enemman_kuin_saldo(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
